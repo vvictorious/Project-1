c284385789e0e78ef5a96d8a7aeef0060b567e06
vvictorious <vvictorious30@gmail.com>
2018-04-06 12:58:59 -0600

    got an explosion to show up on a click.  not where I want it to show up though.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 let background = document.getElementById("background_for_click");
+
+
 background.onmouseover= function() {
 	this.style.cursor="crosshair";
 };
+
+background.onclick = function() {
+	// console.log('ya did it!');
+	document.getElementById("explosion").style.display = "block";
+}
+
+
+// function shoot_animation(){
+// let shots_fired = document.getElementById("animation_for_click")
+// let pos = 0;
+// let id =setInterval(frame, 10);
+// 	function frame() {
+// 		background.addEventListener("")
+// 		if (background.)
+// 	}
+// }
+
+
+
+// background.addEventListener("click", )
