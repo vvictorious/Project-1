cb34417fe84861069c0df6873008f97491401c50
vvictorious <vvictorious30@gmail.com>
2018-04-08 18:44:14 -0600

    got a target to move

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 let background = document.getElementById("background_for_click");
+let target= document.getElementById("target");
 
 
-background.onmouseover= function() {
-	this.style.cursor="crosshair";
-};
+ background.onmouseover= function() {
+ 	this.style.cursor="crosshair";
+}; 
 
-background.onclick = function() {
-	// console.log('ya did it!');
-	document.getElementById("explosion").style.display = "block";
+
+
+
+background.onmouseover= function myMove(){
+	 background.onmouseover= 
+ 	this.style.cursor="crosshair";
+ 	// var elem =document.getElementById("target");
+	var pos = 0;
+	var id = setInterval(frame, 10);
+	function frame () {
+		if (pos ==350) {
+			clearInterval(id);
+	} else {
+		pos++;
+		target.style.top = pos + 'px';
+		target.style.left = pos + 'px';
+	}
+	}
 }
 
-
-// function shoot_animation(){
-// let shots_fired = document.getElementById("animation_for_click")
-// let pos = 0;
-// let id =setInterval(frame, 10);
-// 	function frame() {
-// 		background.addEventListener("")
-// 		if (background.)
-// 	}
-// }
+//check out tween max
 
 
 
-// background.addEventListener("click", )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
