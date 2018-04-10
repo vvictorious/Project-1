cb47b371d9063154df360d0cac1a6b8b58953ba1
vvictorious <vvictorious30@gmail.com>
2018-04-09 21:19:24 -0600

    Score is updating on click, but only on one click for some reason.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+
+
 let background = document.getElementById("background_for_click");
 let target= document.getElementById("target");
+let score = document.getElementById("score");  //when I add.value; the target stops moving
+
+
+
+
+
 
 
 
@@ -9,14 +17,15 @@ background.onmouseover= function myMove(){
  }
 
 
-//this is the function that makes the target move 
+//this is the function that makes the target move
+//called frame 
 function frame() {
 	
-	//by play area is 700 by 700 pixels
+	//my play area is 700 by 700 pixels
 	//thereore, these variables make the dimensions for 
 	//the targets to move around randomly in
-	var random_margin_top = 670 * Math.random();
-	var random_margin_left = 670 * Math.random();
+	var random_margin_top = 650 * Math.random();
+	var random_margin_left = 650 * Math.random();
 	//these 2 lines assigning the dimensions to the CSS
 	//and adding pixels so that it will actually work.
 	target.style.marginTop =  random_margin_top + 'px';
@@ -24,15 +33,29 @@ function frame() {
 }
 
 
-//this is calling the frame function every 1000 milliseconds
-setInterval(frame, 900);
+//this is calling the frame function every how ever many milliseconds
+setInterval(frame, 2000);
 
 //this is what's "destroying the target"
 target.onclick = function destroy() {
 	target.style.visibility = 'hidden';
+	//the setTimeout is the function that makes the target reappear
+	//every how ever many seconds.
 	setTimeout(makeVisible, 1000);
+	if (target.onclick=true) {
+		score += 10;
+	}
+		var lblScore = document.getElementById('score_tracker');
+		score_tracker.innerHTML=score;
+	
+
 };
 
+//I am calling to the makeVisible function to make its 
+//CSS style visible.  This part was confusing to me, but I get it 
+//now.  I made the makeVisible function with the two arguments
+//in the destroy function.  I need to call for it in another 
+//function with what it is going to do.
 function makeVisible(){
 	target.style.visibility = "visible"
 }
@@ -42,10 +65,20 @@ function makeVisible(){
 
 
 
-// function respawn(){
-// 	setTimeout(target.style.visibility = 'block', 1000);
+
+// let score = document.getElementById("lblscore").value;
+// function updatescore (value) {
+// 	if(target.onclick = true) {
+// 		score += 10;
+// 	}
+
 // };
 
+// if (target.onclick=true){
+// 	score += 10;
+// }
+// var lblScore = document.getElementById('lblScore');
+// lblScore.innerHTML=score;
 
 
 
@@ -55,24 +88,6 @@ function makeVisible(){
 
 
 
-
-
-
-
-
-
-
-
-// setTimeout(respawn, 3000){
-// 	target.style.display = 'block';
-// };
-
-
-
-
-// target.onclick = function destroy() {
-// 	target.parentNode.removeChild(target);
-// };
 
 
 
