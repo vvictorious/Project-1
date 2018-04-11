d7af648c3f37c8d7dad2b9d2e89cbd6471dc3ac0
vvictorious <vvictorious30@gmail.com>
2018-04-11 07:56:32 -0600

    done, for now

--- app.js
+++ app.js
@@ -1,72 +1,53 @@
 
 
 let background = document.getElementById("background_for_click");
-// let target= document.getElementsByClassName("target");
 let score = document.getElementById("score");  //when I add.value; the target stops moving
 // console.log(target);
 let start =document.getElementById("startButton");
-
 let title = document.getElementById("title");
 let description = document.getElementById("description");
-
-
-
-
-console.log(start);
-
+// console.log(start);
 let timeleft = 30;
 
-//this is my function that turns my mouse into a crosshair		
-background.onmouseover= function myMove(){
+background.onmouseover= function myMove(){  //this is my function that turns my mouse into a crosshair		
 	this.style.cursor="crosshair";
  }
 
+//this is where I do the majority of my on click functions
 
  start.onclick = function (){
- 	console.log('start button');
- 	let centerBox =document.getElementById("gameInfo");
- 	centerBox.style.display = "none";
- 	document.getElementById("target_container").style.display = 'inline-block';
- 	var downloadTimer = setInterval(function(){
-	timeleft--;
- 	document.getElementById("countdown").textContent = timeleft;
+ 	// console.log('start button');
+ 	let centerBox =document.getElementById("gameInfo");  //parent div that holds instruction
+ 	centerBox.style.display = "none";  //on click make that parent div invisible
+ 	document.getElementById("target_container").style.display = 'inline-block';  //on click make the container that holds the target visible/appear
+ 	var downloadTimer = setInterval(function(){  //this is the timer function
+	timeleft--;  //telling the function to increment downwards
+ 	document.getElementById("countdown").textContent = timeleft;  //getting the text content in my paragraph tag
   	if(timeleft <= 0){
-  		alert("You scored " + score + " points");
-    	clearInterval(downloadTimer);
-    	document.getElementById('target_container').style.display='none';
-    	centerBox.removeChild(description);
-    	centerBox.removeChild(start);
-    	centerBox.style.display = "block";
+  		alert("You scored " + score + " points");  //this is what happens when time reaches zero
+    	clearInterval(downloadTimer);  //stops the timer
+    	document.getElementById('target_container').style.display='none';  //this hides the target
+    	centerBox.removeChild(description);  //this removes the instructions
+    	centerBox.removeChild(start);  //this removes the start button
+    	centerBox.style.display = "block";  //this makes my box come back
 
 
     }
-},1000);
+},1000);  //this is for the timer 
  	
  }
 
 
-
-
-
-
-
-
-
-
-
-//i need target_container to stop
-
+//this is where the target gets made
 function createTarget(){
-	let target =document.createElement("img")
-	target.setAttribute("id", "targetObject")
-	target.setAttribute("src", "dart-board.png")	
-	target.addEventListener("click", addScore)
-	let targetContainer = document.getElementById("target_container");
-	targetContainer.appendChild(target);
+	let target =document.createElement("img")  //setting target
+	target.setAttribute("id", "targetObject")  //setting the id to targetObject
+	target.setAttribute("src", "dart-board.png")  //getting the photo that will be the target
+	target.addEventListener("click", addScore)  //adding an on click event listener to add score
+	let targetContainer = document.getElementById("target_container");  
+	targetContainer.appendChild(target);   //appending the target 
 	return target;
 }
-
-
 
 
 function randomize(dart){
@@ -82,10 +63,6 @@ function randomize(dart){
 }
 
 
-
-
-
-
 //this is the function that makes the target respawn called frame
 //this is storing the create target function from above as a variable while calling that function
 //then we are calling the randomize function with the first target variable as a parameter
@@ -93,23 +70,21 @@ function randomize(dart){
 function frame() {
 	let firstTarget = createTarget();
 	randomize(firstTarget);
-	setInterval(randomize, 900, firstTarget);
+	setInterval(randomize, 950, firstTarget);  //this is where I control the speed of the target
 	// console.log(firstTarget);
 }
 
 
+frame()  //this is calling frame right above
 
-
-//this is calling frame right above
-frame()
 
 function addScore() {
-	let oldTarget = document.getElementById("targetObject")
-	oldTarget.parentNode.removeChild(oldTarget);
-	score += 10;
-	var lblScore = document.getElementById('score_tracker');
-	score_tracker.innerHTML=score;
-	frame();
+	let oldTarget = document.getElementById("targetObject")  //setting old target within scope
+	oldTarget.parentNode.removeChild(oldTarget); //targetcontainer is the parent node, thereforeremove child removes the target object 
+	score += 10;  //when this happens add ten points
+	var lblScore = document.getElementById('score_tracker');  //get score tracker and store as variable in scope
+	score_tracker.innerHTML=score; //put that score to the inner html
+	frame();  //call frame again to make another target
 }
 
 
